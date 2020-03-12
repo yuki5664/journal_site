7fb332529ce461b72187883271ee4bddd0f40cf4
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-12 12:33:41 +0900

    アプリ修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    redirect_to @article
+    redirect_to articles_path
   end
 
   def edit
